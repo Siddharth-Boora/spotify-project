acc431c444de83fca42ba24e7b566b40d9f99c68
Siddharth-Boora <sidboora194@gmail.com>
2025-06-06 12:25:04 -0700

    Made buttons for library section and made data.

---
+++ spotify-project/data/library-data.js
@@ -0,0 +1,12 @@
+export const libData = [
+  {
+    name: "Lil Tecca",
+    img: "images/liltecca.png",
+    type: "Artist"
+  },
+  {
+    name: "radiohead favorites",
+    img: "radioheadplaylist.png",
+    type: "Playlist"
+  }
+]
